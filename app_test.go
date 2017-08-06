3a9a959df52811b8314744719da73be137b24d2c
Clint Edwards <clint.j.edwards@gmail.com>
2017-08-06 01:55:48 -0400

    Adding app file unit tests

--- app_test.go
+++ app_test.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
+
+func TestNewApp(t *testing.T) {
+	newapp := newApp()
+
+	t.Run("is not empty", func(t *testing.T) {
+		if newapp.Token == "" {
+			t.Fatal("token field for struct app empty")
+		}
+	})
+
+	t.Run("correct length", func(t *testing.T) {
+		if len(newapp.Token) != 20 {
+			t.Log(newapp.Token)
+			t.Fatalf("expected token length 10; got length %d", len(newapp.Token))
+		}
+	})
+}
 
 func TestIsAuthorizedUser(t *testing.T) {
 	app := &app{"testapp", "https://localhost", []string{"barack.obama@gmail.com"}, "somerandomstringofcharacters"}
@@ -31,28 +51,50 @@ func TestIsAuthorizedUser(t *testing.T) {
 
 func TestSendMessageApproval(t *testing.T) {
 
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-	}))
-	defer testServer.Close()
+	t.Run("server recieves correct message format", func(t *testing.T) {
+		var callbackInfo = make(map[string]interface{})
 
-	var callbackInfo = make(map[string]interface{})
+		callbackInfo["app_name"] = "testapp"
+		callbackInfo["message_id"] = "randomstringofchars"
+		callbackInfo["test_info"] = "nothingofsubstance"
 
-	callbackInfo["app_name"] = "testapp"
-	callbackInfo["message_id"] = "randomstringofchars"
-	callbackInfo["test_info"] = "nothingofsubstance"
+		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			receivedJSON, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	app := &app{"testapp", testServer.URL, []string{"barack.obama@gmail.com"}, "somerandomstringofcharacters"}
+			var receivedValues map[string]interface{}
+			json.Unmarshal([]byte(receivedJSON), &receivedValues)
 
-	t.Run("approval successfully sent", func(t *testing.T) {
+			for key := range callbackInfo {
+				if callbackInfo[key] != receivedValues[key] {
+					t.Fatalf("recieved value %s for key %s different from expected value %s", callbackInfo[key], key, receivedValues[key])
+				}
+			}
+
+		}))
+
+		defer testServer.Close()
+
+		app := &app{"testapp", testServer.URL, []string{"barack.obama@gmail.com"}, "somerandomstringofcharacters"}
+
 		err := app.sendMessageApproval(callbackInfo, "barack.obama@gmail.com", true)
 		if err != nil {
 			t.Fatal(err)
 		}
 	})
 
-	t.Run("correct json params sent", func(t *testing.T) {
-
+	t.Run("returns error if server doesn't respond", func(t *testing.T) {
+		var callbackInfo = make(map[string]interface{})
+		app := &app{"testapp", "http://localhost", []string{"barack.obama@gmail.com"}, "somerandomstringofcharacters"}
+		err := app.sendMessageApproval(callbackInfo, "barack.obama@gmail.com", true)
+		if err != nil {
+			if !strings.Contains(err.Error(), "connection refused") {
+				t.Fatalf("incorrect error response; expected body contains connection refused got %s", err)
+			}
+		}
 	})
 
 }
